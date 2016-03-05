35ca56c6c3357b1df71553c99040f17fea91e83a
barryWarnock <brrwarnock@gmail.com>
2016-03-04 18:53:29 -0800

    I have added the following classes CPU_6502: 	an implementation of the 6502 cpu used in a NES Memory: 	an interface that allows us to handle the strange memory mirroring 	found in the meory of a NES CPU_Memory: 	an implementation of the memory that is attached to the cpu in a NES NES: 	a collection of hardware classes that I think should probably be 	in charge of telling the cpu to execute instructions at the 	correct clock intervals as well as loading rom files into memory
    The one thing I am unsure of is how we want to store instructions,
    the way I see it we have two options:
    	1: keep a map of Command objects with an execute method keyed by 
    	opcode (much like our IDIOT implementation)
    	2: a switch statement that calls a different function in 
    	CPU_6502 for each opcode (this is the system I have loosley
    	implemented in this commit but I'm not married to it so if either of
    	you would prefer option 1 or something else feel free to say so)

---
+++ cpsc231Emulator/src/cpsc231emulator/CPU_6502.java
@@ -0,0 +1,87 @@
+package cpsc231emulator;
+
+/**
+ * an implementation of the MOS 6502 processor used in the NES which
+ * had none of the 6502's BCD operations
+ * documentation of the 6502's opcodes can be found at the following links:
+ * http://6502.org/tutorials/6502opcodes.html
+ * http://www.visual6502.org/wiki/index.php?title=6502_all_256_Opcodes
+ * other useful documents at:
+ * https://en.wikipedia.org/wiki/MOS_Technology_6502#Registers
+ * http://nesdev.com/NESDoc.pdf
+ */
+public class CPU_6502 {
+    //a reference to the NES's memory
+    protected Memory mem;
+    /*
+    * define the registers
+    * X,Y 8 bit index registers
+    * S stack pointer
+    * PC program counter
+    */
+    protected short X, Y, A, S;
+    protected int PC;
+    /*
+    * define the flags
+    * C carry
+    * Z zero
+    * I interupt disable
+    * D decimal mode (not implemented)
+    * B break command
+    * V overflow
+    */
+    protected boolean C, Z, I, B, V;
+    
+    public void reset() {
+        X = Y = A = S= 0;
+        PC = (mem.read(0xFFFD)*256)+mem.read(0xFFFC); //set PC to reset vector
+        
+        C = Z = I = B = V = false;
+    }
+    
+    public CPU_6502(Memory newMem) {
+        mem = newMem;
+        reset();
+    }
+    
+    /**
+     * returns the number of clock cycles the next instruction will take
+     * this is important because the PPU (picture processing unit)
+     * runs along side the cpu so they need to stay in sync
+     * @return the number of clock cycles the next operation will take
+     */
+    public byte fetch() {
+        switch (mem.read(PC)) {
+            case 0x00:
+                return 0;
+            case 0x01:
+                return 6;
+            default:
+                return -1;
+        } //TODO write a script to generate the cases for all opcodes (unless someone wants to manually copy the 150+ permutations)
+        //this should probably actually end up in a config file that gets loaded into a map but we can talk about that later
+    }
+    
+    /**
+     * executes the next operation and increments the PC
+     */
+    public void execute() {
+        switch (mem.read(PC)) {
+            case 0x00:
+                op_00();
+                break;
+            default:
+                //log unimplemented op-code
+        }
+    }
+    
+    /**
+     * BRK causes a non-maskable interrupt and increments the program counter by 
+     * one. Therefore an RTI will go to the address of the BRK +2 so that BRK may 
+     * be used to replace a two-byte instruction for debugging and the subsequent 
+     * RTI will be correct.
+     */
+    protected void op_00() {
+        PC++;
+    }
+}

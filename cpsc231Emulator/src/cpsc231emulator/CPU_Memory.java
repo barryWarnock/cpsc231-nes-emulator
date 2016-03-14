2bdac281f2db442aa75965199e64b1c60124176c
JeremyBolster <bolster@Jeremys-MacBook-Pro.local>
2016-03-13 17:30:30 -0700

    Added PPU Memory Class
    I added a PPU memory class and added in the write method. I also
    tweaked the CPU_Memory class to make it more legible. This entailed
    removing awkward while loops in favour of modulus operations.

--- cpsc231Emulator/src/cpsc231emulator/CPU_Memory.java
+++ cpsc231Emulator/src/cpsc231emulator/CPU_Memory.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cpsc231emulator;
 
 /**
@@ -13,7 +8,7 @@ public class CPU_Memory implements Memory{
     protected short[] memArray;
     
     public CPU_Memory() {
-        memArray = new short[65536];
+        memArray = new short[0x10000];
     }
     
     @Override
@@ -22,39 +17,51 @@ public class CPU_Memory implements Memory{
      */
     public short read(int address) {
         return memArray[address];
+        //TODO input validation
     }
 
     @Override
     /**
      * @inheritDoc
-     * 0x0000-0x07FF is mirrored four times
-     * 0x2008-0x2007 is mirrored 1023 times
+     * @author bolster
+     * magic number time!
+      * base    size    purpose
+     * $0000   $800    2KB of work RAM
+     * $0800   $800    Mirror of $000-$7FF
+     * $1000   $800    Mirror of $000-$7FF
+     * $1800   $800    Mirror of $000-$7FF
+     * $2000   8       PPU Ctrl Registers
+     * $2008   $1FF8   *Mirror of $2000-$2007
+     * $4000   $20      Registers (Mostly APU)
+     * $4020   $1FDF   Cartridge Expansion ROM
+     * $6000   $2000   SRAM
+     * $8000   $4000   PRG-ROM
+     * $C000   $4000   PRG-ROM
      */
     public void write(int address, short value) {
-        //the 2kb of ram mirrored 4 times
-        if (address < 0x2000) {
-            /*
-            * make the address fit in the first 0x800 of memory (to make 
-            * calculating the addresses for the mirrored sections easier)
-            */
-            while (address > 0x7FF) {
-                address -= 0x800;
-            }
-            for (int i = 0; i < 4; i++) {
-                memArray[address + (i*0x800)] = value;
+
+        //Mirroring of first memory chunk
+        if(0x2000 > address) {
+
+            int baseAddress = address % 0x800; //"base" address
+            memArray[baseAddress] = value; //first mirror
+            memArray[baseAddress + 0x800] = value; //second mirror
+            memArray[baseAddress + (0x800 * 2)] = value; //third mirror
+            memArray[baseAddress + (0x800 * 3)] = value; //fourth mirror
+        }
+        //Mirroring of second memory chunk
+        else if(0x4000 > address) {
+            /* There are only 8 PPU control registers */
+            int baseAddress = (address - 0x2000) % 8; //"base" address
+            for(int i = 0; 1023 > i; i++) {
+                //1023 * 8 = 0x2000 Therefore all of the mirrored values are covered
+                memArray[0x2000 + baseAddress + (i * 8)] = value;
             }
         }
-        else if (address < 0x4000) { //address > 0x2000 satisfied by the previous if
-            //force memory into the 0x2000-0x2007 range
-            while (address > 0x2007) {
-                address -= 0x8;
-            }
-            for (int i = 0; i < 1024; i++) {
-                memArray[address + (i*0x800)] = value;
-            }
-        }
+        //No mirroring for higher memory locations
         else {
             memArray[address] = value;
+            //TODO input validation
         }
     }
 }
